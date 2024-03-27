c7a68dc89c139b47f3bd7a0965116459df14546c
SW49GO <gohard.so49@gmail.com>
2024-03-27 06:59:16 +0100

    update Readme

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 ## Presentation :
 This is a reusable React component that allows you to display a custom Table in your application.
 
-The component is TypeScript compatible and includes a type definition file (TableReact.d.ts) for an improved development experience. In a TypeScript project, the TypeScript compiler will automatically use this definition file.
+In this table :
+- You can choose the number of lines to display
+- You can carry out a search on each column by first clicking on the column header
+- The data display can be sorted by column in ascending or descending order by clicking on the desired column header
+- You have the possibility, using the 'allowRemoveRow' props, to make an icon appear on each line to retrieve its contents using the associated callback function 'handleRemoveRow'
+
+##### The component is TypeScript compatible and includes a type definition file (TableReact.d.ts) for an improved development experience. In a TypeScript project, the TypeScript compiler will automatically use this definition file.
 
 ### Examples Custom style :
 <img src="https://raw.githubusercontent.com/SW49GO/React-Table/master/public/assets/example.jpg" alt="datepicker"/>
@@ -23,10 +29,13 @@ npm i table-react-sw49go
 
 ## Format of data :
 ```
-// key 'name' for title thead & 'sort' initialize the expected behavior of sorting data, REQUIRED
-// AZ : alphanumeric
-// NUM : number
-// DATE: format date dd/mm/yyyy 
+/** COLUMNS CONFIGURATION :
+*   keys: 'name' for title thead & 'sort' initialize the expected behavior of sorting data, REQUIRED
+*   values : 'AZ' for alphanumeric
+*            'NUM' for number
+*            'DATE' format for date dd/mm/yyyy 
+*/
+
 
     const columns = [
         {name:'Fruits',sort:'AZ'},
@@ -34,18 +43,19 @@ npm i table-react-sw49go
         {name:'Date d\'achat',sort:'DATE'},
         {name:'Price',sort:'NUM'}
     ]
+
     const data =[
         { fruit: 'Banane', flower:'Tulipe', date:'10/02/2024', price:10},
         { fruit: 'Orange', flower:'Rose', date:'07/01/2024', price:25}]
 
-// Number entries NOT REQUIRED, default is :
+// Number entries NOT REQUIRED but can be changed, default is :
 
     const entries = ['5','10','15','20','50','100']
 ```
 
 ## Imported the component into your project :
 ```
-import { TableReact} from 'table-react-sw49go'
+import { TableReact } from 'table-react-sw49go'
 
 // IMPORT YOUR DATA
 
@@ -55,11 +65,13 @@ function App(){
     const handleNbEntries=(nbEntries)=>{
         console.log('nbEntries:', nbEntries)
     }
+
 // Function to retrieve search generated object
     const handleResultSearch= (result)=>{
         console.log('result:', result)
     }
-// Function to retrieve the row selected 'to be remove or other' and his index if allowRemoveRow be 'true'
+
+// Function to retrieve the row selected 'to be remove or other' and his index if 'allowRemoveRow' be 'true'
     const handleRemoveRow= (objRemove, index)=>{
         console.log('index:', index)
         console.log('objRemove:', objRemove)
--- src/example/App.js
+++ src/example/App.js
@@ -3,34 +3,27 @@ import TableReact from "../lib/TableReact"
 
 function App(){
 
-    // const columns = [
-    //     {name:'First Name',sort:'AZ'},
-    //     {name:'Last Name',sort:'AZ'}, 
-    //     {name:'Date of Birth',sort:'DATE'},
-    //     {name:'Start Date',sort:'DATE'},
-    //     {name:'Department',sort:'AZ'},
-    //     {name:'Street',sort:'AZ'},
-    //     {name:'City',sort:'AZ'},
-    //     {name:'State',sort:'AZ'},
-    //     {name:'Zip Code',sort:'NUM'}]
     const columns = [
-        {name:'Fruits',sort:'AZ'},
-        {name:'Fleurs',sort:'AZ'}, 
-        {name:'Date d\'achat',sort:'DATE'},
-        {name:'Prix',sort:'NUM'}
-    ]
+        {name:'First Name',sort:'AZ'},
+        {name:'Last Name',sort:'AZ'}, 
+        {name:'Date of Birth',sort:'DATE'},
+        {name:'Start Date',sort:'DATE'},
+        {name:'Department',sort:'AZ'},
+        {name:'Street',sort:'AZ'},
+        {name:'City',sort:'AZ'},
+        {name:'State',sort:'AZ'},
+        {name:'Zip Code',sort:'NUM'}]
+
     const data =[
-        { fruit: 'Banane', fleur:'Tulipe', date:'10/02/2024', prix:10},
-        { fruit: 'Orange', fleur:'Rose', date:'07/01/2024', prix:25}]
-    // const employeesData =[
-    //     { firstName: 'Lea' , lastName: 'Laravel' , dateOfBirth: '02/10/1970', startDate: '10/01/2024' ,department: 'Engineering', street:'Verdun', city: 'Angers' ,states:'AL', zipCode: '49000'},
-    //     { firstName: 'Etienne' , lastName: 'Daho' , dateOfBirth: '20/07/1973', startDate: '17/02/2024' ,department: 'Sales', street:'Charles', city: 'Nantes' ,states:'AK', zipCode: '44000'},
-    //     { firstName: 'John' , lastName: 'Portman' , dateOfBirth: '07/03/1975', startDate: '11/03/2024' ,department: 'Marketing', street:'Minier', city: 'Montauban' ,states:'CA', zipCode: '82000'},
-    //     { firstName: 'Marie' , lastName: 'Goldman' , dateOfBirth: '10/04/1965', startDate: '23/05/2024' ,department: 'Legal', street:'Hotel bleu', city: 'Cholet' ,states:'ID', zipCode: '49000'},
-    //     { firstName: 'Paul' , lastName: 'Newman' , dateOfBirth: '06/10/1963', startDate: '21/07/2024' ,department: 'Sales', street:'Des motos', city: 'Lyon' ,states:'FL', zipCode: '87000'},
-    //     { firstName: 'Bradley' , lastName: 'Cooper' , dateOfBirth: '15/103/1971', startDate: '29/07/2024' ,department: 'Sales', street:'Gemmes', city: 'Nantes' ,states:'FL', zipCode: '44000'}
-    // ]
-    //const entries = ['5','10','15','20','50','100']
+        { firstName: 'Lea' , lastName: 'Laravel' , dateOfBirth: '02/10/1970', startDate: '10/01/2024' ,department: 'Engineering', street:'Verdun', city: 'Angers' ,states:'AL', zipCode: '49000'},
+        { firstName: 'Etienne' , lastName: 'Daho' , dateOfBirth: '20/07/1973', startDate: '17/02/2024' ,department: 'Sales', street:'Charles', city: 'Nantes' ,states:'AK', zipCode: '44000'},
+        { firstName: 'John' , lastName: 'Portman' , dateOfBirth: '07/03/1975', startDate: '11/03/2024' ,department: 'Marketing', street:'Minier', city: 'Montauban' ,states:'CA', zipCode: '82000'},
+        { firstName: 'Marie' , lastName: 'Goldman' , dateOfBirth: '10/04/1965', startDate: '23/05/2024' ,department: 'Legal', street:'Hotel bleu', city: 'Cholet' ,states:'ID', zipCode: '49000'},
+        { firstName: 'Paul' , lastName: 'Newman' , dateOfBirth: '06/10/1963', startDate: '21/07/2024' ,department: 'Sales', street:'Des motos', city: 'Lyon' ,states:'FL', zipCode: '87000'},
+        { firstName: 'Bradley' , lastName: 'Cooper' , dateOfBirth: '15/103/1971', startDate: '29/07/2024' ,department: 'Sales', street:'Gemmes', city: 'Nantes' ,states:'FL', zipCode: '44000'}
+    ]
+
+
     const handleNbEntries=(nbEntries)=>{
         console.log('nbEntries:', nbEntries)
     }
@@ -43,7 +36,7 @@ function App(){
     }
 
     return (
-        <div>
+        <div style={{width:'60rem'}}>
             <TableReact dataColumns={columns}
                         dataAllRows={data}
                         handleNbEntries={handleNbEntries}
